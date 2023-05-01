779393306c0402aba6534729cb96f698a1d32629
rocketman123456 <759094438@qq.com>
2023-05-01 23:00:31 +0800

    update format

--- SPIne-8M/math_ops.h
+++ SPIne-8M/math_ops.h
@@ -9,8 +9,8 @@ float fmaxf(float x, float y);
 float fminf(float x, float y);
 float fmaxf3(float x, float y, float z);
 float fminf3(float x, float y, float z);
-void limit_norm(float *x, float *y, float limit);
-int float_to_uint(float x, float x_min, float x_max, int bits);
+void  limit_norm(float* x, float* y, float limit);
+int   float_to_uint(float x, float x_min, float x_max, int bits);
 float uint_to_float(int x_int, float x_min, float x_max, int bits);
 
 #endif
