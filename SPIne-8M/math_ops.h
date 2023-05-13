6a07539d5703b92c299f706fb25f06a1ddfdca90
rocketman123456 <759094438@qq.com>
2023-05-13 18:47:48 +0800

    format code

--- SPIne-8M/math_ops.h
+++ SPIne-8M/math_ops.h
@@ -1,5 +1,4 @@
-#ifndef MATH_OPS_H
-#define MATH_OPS_H
+#pragma once
 
 #define PI 3.14159265359f
 
@@ -9,8 +8,6 @@ float fmaxf(float x, float y);
 float fminf(float x, float y);
 float fmaxf3(float x, float y, float z);
 float fminf3(float x, float y, float z);
-void limit_norm(float *x, float *y, float limit);
-int float_to_uint(float x, float x_min, float x_max, int bits);
+void  limit_norm(float* x, float* y, float limit);
+int   float_to_uint(float x, float x_min, float x_max, int bits);
 float uint_to_float(int x_int, float x_min, float x_max, int bits);
-
-#endif
